74ac7a28812885c68bfe596d1aacae0aff983c8e
Snigdho Dip Howlader <61914319+snigdho611@users.noreply.github.com>
2020-10-24 19:54:55 +0600

    Update

---
+++ URI Online Judge/BEGINNER/1564 - Brazil World Cup.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        inp = int(input())
+        if inp > 0:
+            print("vai ter duas!")
+        else:
+            print("vai ter copa!")
+    except EOFError:
+        break    
