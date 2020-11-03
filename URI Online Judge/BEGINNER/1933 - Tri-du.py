b106072b3a4a4100d7c2b8e5dafdf2cb6178036d
Snigdho Dip Howlader <61914319+snigdho611@users.noreply.github.com>
2020-11-03 11:37:02 +0600

    python problems

---
+++ URI Online Judge/BEGINNER/1933 - Tri-du.py
@@ -0,0 +1,11 @@
+x, y = input().split()
+x = int(x)
+y = int(y)
+
+if x > y:
+    print(x)
+elif x < y:
+    print(y)
+elif x == y:
+    print(x)
+    
---
+++ URI Online Judge/BEGINNER/1957 - Converting to Hexadecimal.py
@@ -0,0 +1,7 @@
+inp = int(input())
+
+inp = hex(inp)
+inp = inp.upper()
+inp = inp[2:]
+
+print(inp)
