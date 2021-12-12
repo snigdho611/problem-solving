f2b96c19ee5be627c274f3beaed29f3800527fc4
snigdho611 <snigdho.howlader@gmail.com>
2021-12-13 04:47:53 +0600

    Solved 10 days of JS - throw

---
+++ HackerRank/10DaysOfJS/throw.js
@@ -0,0 +1,20 @@
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    if(a > 0){
+        return "YES"
+    }else if(a < 0){
+        return "Negative Error"
+    }
+    else{
+        return "Zero Error"
+    }
+}
+
+let val = 0 //Enter your value here
+
+console.log(isPositive(val))
