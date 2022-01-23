f365a175001405b3c8d8569b62775cba7347e706
snigdho611 <snigdho.howlader@gmail.com>
2022-01-23 22:43:57 +0600

    Updating repository for problem solving

---
+++ HackerRank/30DaysOfCode/4-classes-vs-instances.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 7;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << x << " x " << i << " = " << x * i << endl;
+    }
+
+    return 0;
+}
