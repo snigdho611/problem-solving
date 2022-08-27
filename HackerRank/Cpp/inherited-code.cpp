eecc1139af2afb6d323b10a49f6aa1ac19f34aef
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-08-27 17:02:09 +0600

    Create inherited-code.cpp

---
+++ HackerRank/Cpp/inherited-code.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Define the exception here */
+class BadLengthException : public std::exception {
+   private:
+    int x;
+
+   public:
+    BadLengthException(int n) {
+        // char* what() { return "Hey"; }
+        x = n;
+    }
+
+    int what() { return x; }
+};
+
+bool checkUsername(string username) {
+    bool isValid = true;
+    int n = username.length();
+    if (n < 5) {
+        throw BadLengthException(n);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        if (username[i] == 'w' && username[i + 1] == 'w') {
+            isValid = false;
+        }
+    }
+    return isValid;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string username;
+        cin >> username;
+        try {
+            bool isValid = checkUsername(username);
+            if (isValid) {
+                cout << "Valid" << '\n';
+            } else {
+                cout << "Invalid" << '\n';
+            }
+        } catch (BadLengthException e) {
+            cout << "Too short: " << e.what() << '\n';
+        }
+    }
+    return 0;
+}
