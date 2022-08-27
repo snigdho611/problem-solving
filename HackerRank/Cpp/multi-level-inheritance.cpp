6988449c915565dfa8ec921f87200e5beaa6f968
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-08-27 10:53:59 +0600

    Create multi-level-inheritance.cpp

---
+++ HackerRank/Cpp/multi-level-inheritance.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Triangle {
+   public:
+    void triangle() { cout << "I am a triangle\n"; }
+};
+
+class Isosceles : public Triangle {
+   public:
+    void isosceles() { cout << "I am an isosceles triangle\n"; }
+};
+
+class Equilateral : public Isosceles {
+   public:
+    void equilateral() { cout << "I am an equilateral triangle\n"; }
+};
+
+int main() {
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
+}
