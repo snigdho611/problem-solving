b4022bd6d962b7b16108979b1b17d1c729dabcca
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-08-27 10:53:57 +0600

    Create variable-sized-arrays.cpp

---
+++ HackerRank/Cpp/variable-sized-arrays.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, queries;
+    cin >> n >> queries;
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int length;
+        cin >> length;
+        arr[i].resize(length);
+        for (int j = 0; j < length; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int k = 0; k < queries; k++) {
+        int i, j;
+        cin >> i >> j;
+        cout << arr[i][j] << endl;
+    }
+    return 0;
+}
