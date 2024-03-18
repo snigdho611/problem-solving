5e4beb7b1da3881a9b603bc03c336be0aaad1054
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2024-03-18 15:47:52 +0600

    Adding further solutions

--- leetcode/13-roman-to-integer.cpp
+++ leetcode/13-roman-to-integer.cpp
@@ -11,114 +11,26 @@ public:
         int total = 0;
         int i = 0;
 
+        map<char, int> roman_to_int;
+        roman_to_int['I'] = 1;
+        roman_to_int['V'] = 5;
+        roman_to_int['X'] = 10;
+        roman_to_int['L'] = 50;
+        roman_to_int['C'] = 100;
+        roman_to_int['D'] = 500;
+        roman_to_int['M'] = 1000;
+
         while (i < s.length())
         {
-            switch (s[i])
+            if (roman_to_int[s[i]] < roman_to_int[s[i + 1]])
             {
-            case 'I':
+                total = total - roman_to_int[s[i]];
+            }
+            else
             {
-                if (s[i + 1] && s[i + 1] == 'V')
-                {
-                    cout << "Adding 4..." << endl;
-                    total = total + 4;
-                    i = i + 2;
-                }
-                else if (s[i + 1] && s[i + 1] == 'X')
-                {
-                    cout << "Adding 9..." << endl;
-                    total = total + 9;
-                    i = i + 2;
-                }
-                else
-                {
-                    cout << "Adding 1..." << endl;
-                    total = total + 1;
-                    i = i + 1;
-                }
-                break;
+                total = total + roman_to_int[s[i]];
             }
-
-            case 'V':
-            {
-                cout << "Adding 5..." << endl;
-                total = total + 5;
-                i = i + 1;
-                break;
-            }
-
-            case 'X':
-            {
-                if (s[i + 1] && s[i + 1] == 'L')
-                {
-                    cout << "Adding 50..." << endl;
-                    total = total + 40;
-                    i = i + 2;
-                }
-                else if (s[i + 1] && s[i + 1] == 'C')
-                {
-                    cout << "Adding 90..." << endl;
-                    total = total + 90;
-                    i = i + 2;
-                }
-                else
-                {
-                    cout << "Adding 10..." << endl;
-                    total = total + 10;
-                    i = i + 1;
-                }
-                break;
-            }
-
-            case 'L':
-            {
-                cout << "Adding 50..." << endl;
-                total = total + 50;
-                i = i + 1;
-                break;
-            }
-
-            case 'C':
-            {
-                if (s[i + 1] && s[i + 1] == 'D')
-                {
-                    cout << "Adding 400..." << endl;
-                    total = total + 400;
-                    i = i + 2;
-                }
-                else if (s[i + 1] && s[i + 1] == 'M')
-                {
-                    cout << "Adding 900..." << endl;
-                    total = total + 900;
-                    i = i + 2;
-                }
-                else
-                {
-                    cout << "Adding 100..." << endl;
-                    total = total + 100;
-                    i = i + 1;
-                }
-                break;
-            }
-
-            case 'D':
-            {
-                cout << "Adding 500..." << endl;
-                total = total + 500;
-                i = i + 1;
-                break;
-            }
-
-            case 'M':
-            {
-                cout << "Adding 1000..." << endl;
-                total = total + 1000;
-                i = i + 1;
-                break;
-            }
-
-            default:
-                break;
-            }
+            i++;
         }
 
         return total;
